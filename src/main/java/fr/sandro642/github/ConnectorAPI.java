ca277fa7c2b0a88af009d8fd5ac7bddbedb16eec
Sandro Soria <sandro33810@gmail.com>
2025-07-01 00:44:16 +0200

    Add: initial implementation of JobConnexion class and update ConnectorAPI documentation

--- src/main/java/fr/sandro642/github/ConnectorAPI.java
+++ src/main/java/fr/sandro642/github/ConnectorAPI.java
@@ -2,6 +2,9 @@ package fr.sandro642.github;
 
 /**
  * ConnectorAPI est une librairie Java permettant de créer un lien de communication entre son projet Java et une API Rest Fastify.
+ * @author Sandro642
+ * @version 1.0
+ * @since 1.0
  */
 
 public class ConnectorAPI {
