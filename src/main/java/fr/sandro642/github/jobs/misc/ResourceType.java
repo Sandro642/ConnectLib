8f83d627a09558979d84f1904e5febfc23dd2203
Sandro Soria <sandro33810@gmail.com>
2025-07-08 22:12:33 +0200

    Add: enhance ResourceType enum with additional documentation for resource paths

--- src/main/java/fr/sandro642/github/jobs/misc/ResourceType.java
+++ src/main/java/fr/sandro642/github/jobs/misc/ResourceType.java
@@ -8,15 +8,38 @@ package fr.sandro642.github.jobs.misc;
  */
 
 public enum ResourceType {
+
+    /**
+     * Gradle and Maven Support.
+     */
     MAIN_RESOURCES("src/main/resources"),
     TEST_RESOURCES("src/test/resources");
 
+    /**
+     * Prochainement Support : Minecraft
+     */
+    // MINECRAFT_RESOURCES("src/main/resources/minecraft");
+
+
+    /**
+     * Chemin de la ressource.
+     */
     private final String path;
 
+    /**
+     * Constructeur pour initialiser le chemin de la ressource.
+     *
+     * @param path Le chemin de la ressource.
+     */
     ResourceType(String path) {
         this.path = path;
     }
 
+    /**
+     * Obtient le chemin de la ressource.
+     *
+     * @return Le chemin de la ressource.
+     */
     public String getPath() {
         return path;
     }
